f08e6f0d019bf522bfa1b2c4beb484dbce5a4d2a
ORA <omericoramos@gmail.com>
2024-02-28 18:21:09 -0300

    adicionando logica para a deleção deleteOpeningHandler handler

--- handler/deleteOpeningHandler.go
+++ handler/deleteOpeningHandler.go
@@ -1,14 +1,39 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/omericoramos/vagas-de-emprego/schema"
 )
 
 func DeleteOpeningHandler(ctx *gin.Context) {
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Deletando uma vaga",
-	})
+	// pega o valor do id passado no contexto
+	id := ctx.Query("id")
+
+	// verifica se não esta avzio
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schema.Opening{}
+
+	// verifica se o objeto realmente exite no banco de dados
+	if erro := db.First(&opening, id).Error; erro != nil {
+
+		sendError(ctx, http.StatusNotFound,
+			fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	// deletando os dados do banco
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error deleting opening with id %s", id))
+		return
+	}
+
+	sendSucces(ctx, "delete-opening", opening)
 }
