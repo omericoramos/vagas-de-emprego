091b51c598e9d7ab226165d614276f62d401a5dc
ORA <omericoramos@gmail.com>
2024-02-28 18:53:35 -0300

    adicionando logica para imprimir dados da vaga showOpeningHandler handler

--- handler/showOpeningHandler.go
+++ handler/showOpeningHandler.go
@@ -1,14 +1,29 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/omericoramos/vagas-de-emprego/schema"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Detalhes da vaga cadastrada",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, fmt.Sprintf(errParamIsRequired("id", "queryParameter").Error()))
+		return
+	}
+	opening := schema.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+
+		sendError(ctx, http.StatusNotFound,
+			fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	sendSucces(ctx, "show-opening", opening)
 }
